b2dd4112aebac8733a271867f2fe2242dee8d078
Victor Reipur <victor.reipur@gmail.com>
2023-09-06 11:04:31 +0000

    Deleted files

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 # cd ./app and remove venv if it exits, then rebuild venv, activate it, and install requirements
-cd ./app && rm -rf venv && \
+cd ./app-main && rm -rf venv && \
     python3 -m venv venv && \
     source venv/bin/activate && \
     pip install --upgrade pip && \
@@ -9,3 +9,5 @@ git config --global user.email "victor.reipur@gmail.com"
 git config --global user.name "Victor Reipur"
 git config --global --add safe.directory /usr/src/project
 git config --global --add safe.directory /mnt/project
+
+
