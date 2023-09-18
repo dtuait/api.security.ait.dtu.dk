c463636bd339bde38fb9187c83badf1d67eff975
Victor Reipur <victor.reipur@gmail.com>
2023-09-18 13:17:09 +0000

    adding settings to make app more secure

--- app-main/app/development_settings.py
+++ app-main/app/development_settings.py
@@ -1,3 +1,6 @@
 from .settings import *
 
 DEBUG = True
+
+
+CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
--- app-main/app/settings.py
+++ app-main/app/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 DEBUG = False
 
 
-CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
-# CAS_SERVER_URL = 'https://auth2.dtu.dk/dtu/' # with multifactor
+# CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
+CAS_SERVER_URL = 'https://auth2.dtu.dk/dtu/' # with multifactor
 CAS_VERSION = '2'
 # CAS_REDIRECT_URL = '/admin/'
 
@@ -47,6 +47,13 @@ ALLOWED_HOSTS = [
 ]
 
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600  # This sets the duration for one hour, adjust as needed.
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = False
 # Application definition
 
 INSTALLED_APPS = [
