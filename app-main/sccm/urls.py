73cedee3dae4495afa7efe6cdd6d8adfde45e8a9
Victor Reipur <victor.reipur@gmail.com>
2023-11-30 14:44:03 +0000

    add version number to sccm endpoint

--- app-main/sccm/urls.py
+++ app-main/sccm/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SCCMComputerViewSet
+from .views import SCCMComputerViewSet_1_0_0, SCCMComputerViewSet_1_0_1
 
 router = DefaultRouter()
 # router.register(r'items', ItemViewSet)
@@ -9,7 +9,8 @@ router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('sccm/computer/v1-0-0/<str:computer_name>/', SCCMComputerViewSet.as_view({'get': 'retrieve'})),
+    path('sccm/computer/v1-0-0/<str:computer_name>/', SCCMComputerViewSet_1_0_0.as_view({'get': 'retrieve'})),
+    path('sccm/computer/v1-0-1/<str:computer_name>/', SCCMComputerViewSet_1_0_1.as_view({'get': 'retrieve'})),
     # path('sccm/v2/<str:computer_name>/', SCCMViewSet.as_view({'get': 'retrieve'})),
 ]
 
