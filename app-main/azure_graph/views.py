6fcb724543b257d4a43f8afb2885fc4649f2f08c
Victor Reipur <victor.reipur@gmail.com>
2023-12-01 12:22:13 +0000

    api endpoint now works

--- app-main/azure_graph/views.py
+++ app-main/azure_graph/views.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 import json
-from sccm.scripts.sccm_get_computer_info import get_computer_info
+
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from drf_yasg import openapi
 from rest_framework.exceptions import ParseError
 from .serializers import QuerySerializer
+from .scripts.graph_apicall_runhuntingquery import run_hunting_query
 
 from rest_framework.decorators import action
 
@@ -61,49 +62,18 @@ class GraphHuntingViewSet(viewsets.ViewSet):
 
 
     def run_hunting_query(self, request):
+        
+        # serialize incoming data
         serializer = QuerySerializer(data=request.data)
+        
+        # Check if property query exists
         if serializer.is_valid():
             query = serializer.validated_data['Query']
 
+            result = run_hunting_query(query)
+
             # Now you can use `query` in your code
-            return Response({'Query': query}, status=status.HTTP_200_OK)
+            return Response(result, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-# class GraphHuntingViewSet(viewsets.ViewSet):
-
-
-#     authentication_classes = [TokenAuthentication]  # Require token authentication for this view
-#     permission_classes = [IsAuthenticated]  # Require authenticated user for this view
-
-
-#     header_parameter = openapi.Parameter(
-#         'Authorization',  # name of the header
-#         in_=openapi.IN_HEADER,  # where the parameter is located
-#         description="Type: Token \<token\>",
-#         type=openapi.TYPE_STRING,  # type of the parameter
-#         required=True  # if the header is required or not
-#     )
-
-#     @swagger_auto_schema(
-#         manual_parameters=[header_parameter],
-#         operation_description="""
-#         Hunting query description
-#         """,
-#         responses={
-#             200: 'ComputerInfoSerializer()',
-#             400: 'Error: 1',
-#             404: 'Error: 2',
-#             500: 'Error: Internal server error'
-#         },
-
-#     )
-
-
-
-#     def retrieve(self, request, pk=None):
-
-#         return Response({'message': 'Hello, World!'}, status=status.HTTP_200_OK)
- 
