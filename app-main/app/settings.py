b23bac64c9f8144071f63eb6e859c7d88e422b27
Victor Reipur <victor.reipur@gmail.com>
2023-12-05 11:12:56 +0000

    fix disable debug mode by default

--- app-main/app/settings.py
+++ app-main/app/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
