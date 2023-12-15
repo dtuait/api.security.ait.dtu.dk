53a4b719f990c7cd1156d195a7d1eccd74de2606
Victor Reipur <victor.reipur@gmail.com>
2023-12-15 13:59:31 +0000

    fix debug settings and CSRF coockie domain

---
+++ app-main/app/proxy_debug_false_settings.py
@@ -0,0 +1,19 @@
+from .settings import *
+
+DEBUG = False
+
+
+CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
+
+
+
+
+CSRF_COOKIE_DOMAIN = 'beta-api.security.ait.dtu.dk'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600  # This sets the duration for one hour, adjust as needed.
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = False
