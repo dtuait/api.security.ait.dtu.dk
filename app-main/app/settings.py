cbdb6f85c2c8fd03db76b0420542856e128d3804
Victor Reipur <victor.reipur@gmail.com>
2024-01-26 14:16:11 +0000

    us mariadb instead

--- app-main/app/settings.py
+++ app-main/app/settings.py
@@ -114,13 +114,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app-main',
+        'USER': 'mariadb',
+        'PASSWORD': 'mariadb',
+        'HOST': 'beta-api-security-ait-dtu-dk-mariadb-service',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
