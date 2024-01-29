6f5596611e2b60f62e7fc2d72dc483f3dd09911c
Victor Reipur <victor.reipur@gmail.com>
2024-01-29 15:33:50 +0000

    moving .env to new location

--- app-main/app/settings.py
+++ app-main/app/settings.py
@@ -125,8 +125,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
+<<<<<<< HEAD
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+=======
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST'),
+        'PORT': '3306',
+>>>>>>> f6be410 (moving .env to new location)
     }
 }
 
