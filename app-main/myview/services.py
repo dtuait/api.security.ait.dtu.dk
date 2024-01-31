6a599ace7837ddc0b1abf510d3c12fff3e3b2bfd
Victor Reipur <victor.reipur@gmail.com>
2024-01-31 13:51:06 +0000

    misc

---
+++ app-main/myview/services.py
@@ -0,0 +1,4 @@
+from .scripts.myview_generate_api_token import generate_custom_api_token as _generate_custom_api_token
+
+def generate_custom_api_token(request=None, username=""):
+    return _generate_custom_api_token(request=request, username=username)
