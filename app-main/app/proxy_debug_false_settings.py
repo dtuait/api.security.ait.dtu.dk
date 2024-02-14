4ec5ac4904683b25baf9b8d774d6b9b1ffa8551e
Victor Reipur <vicre@dtu.dk>
2024-02-14 11:30:02 +0000

    patch

--- app-main/app/proxy_debug_false_settings.py
+++ app-main/app/proxy_debug_false_settings.py
@@ -8,7 +8,7 @@ CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
 
 
 
-CSRF_COOKIE_DOMAIN = 'beta-api.security.ait.dtu.dk'
+CSRF_COOKIE_DOMAIN = 'api.security.ait.dtu.dk'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
