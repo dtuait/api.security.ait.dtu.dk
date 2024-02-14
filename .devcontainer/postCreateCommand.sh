2ac8eca90f7e0c7a70b398c7c0dfe5f3a746a3e3
Victor Reipur <vicre@dtu.dk>
2024-02-14 11:30:17 +0000

    no need to remove venv file

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,15 @@
 # cd ./app and remove venv if it exits, then rebuild venv, activate it, and install requirements
-cd ./app-main && rm -rf venv && \
-    python3 -m venv venv && \
-    source venv/bin/activate && \
-    pip install --upgrade pip && \
-    pip install -r requirements.txt
+# cd ./app-main && rm -rf venv && \
+#     python3 -m venv venv && \
+#     source venv/bin/activate && \
+#     pip install --upgrade pip && \
+#     pip install -r requirements.txt
 
-git config --global user.email "victor.reipur@gmail.com"
+cd ./app-main && source venv/bin/activate
+
+git config --global user.email "vicre@dtu.dk"
 git config --global user.name "Victor Reipur"
 git config --global --add safe.directory /usr/src/project
 git config --global --add safe.directory /mnt/project
+git config --global --add safe.directory /usr/src/project/app-main/.docker-migrate
 git config pull.rebase true
