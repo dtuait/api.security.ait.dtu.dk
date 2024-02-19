b6109c1a59e7ebb1eeb0cb96699c3f59e662b575
Victor Reipur <vicre@dtu.dk>
2024-02-19 15:27:50 +0000

    Implemented a script that updates AD structure
    I made a script that updates the script should run every night, or it should run as a singleton process, if a search result is empty, and the format of the search result is not empty, then the script should delete the object from the database, if the search result is not empty, and the format of the search result is empty, then the script should create a new object in the database, if the search result is not empty, and the format of the search result is not empty, then the script should update the object in the database.

--- app-main/myview/admin.py
+++ app-main/myview/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import OrganizationalUnit, UserProfile, EndpointAccess
+from django.db.models import Q
 
 # Registering the OrganizationalUnit model for the admin site.
 @admin.register(OrganizationalUnit)
 class OrganizationalUnitAdmin(admin.ModelAdmin):
-    list_display = ('name',)  # Displaying the 'name' field in the list view.
-    search_fields = ('name',)  # Adding a search bar for the 'name' field.
+    list_display = ('datetime_created', 'datetime_modified', 'distinguished_name', 'canonical_name')
+    search_fields = ('canonical_name',)
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        if search_term:
+            queryset = queryset.filter(canonical_name=search_term)
+        return queryset, use_distinct
 
 # Registering the UserProfile model for the admin site.
 @admin.register(UserProfile)
@@ -25,3 +31,6 @@ class EndpointAccessAdmin(admin.ModelAdmin):
     list_display = ('user', 'endpoint', 'can_access')  # Displaying the fields in the list view.
     list_filter = ('user', 'can_access')  # Adding filters for the 'user' and 'can_access' fields.
     search_fields = ('user__username', 'endpoint')  # Adding a search bar for the 'user__username' and 'endpoint' fields.
+
+
+
