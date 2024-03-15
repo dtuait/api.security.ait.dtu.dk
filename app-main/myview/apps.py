c9a3e505e73cb9ef173826943c12d18fd5b02a15
Victor Reipur <vicre@dtu.dk>
2024-03-15 11:50:33 +0000

    active endpoints auto start enabled

--- app-main/myview/apps.py
+++ app-main/myview/apps.py
@@ -1,9 +1,19 @@
-from django.apps import AppConfig
 
+from django.apps import AppConfig
+from django.core.management import call_command
 
 class MyviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myview'
 
-def ready(self):
-    import signals  # noqa
+
+    def ready(self):
+        # Ensure server is running in development mode before running the script.
+        from django.conf import settings
+        if settings.DEBUG:
+            try:
+                call_command('runscript', 'utils.cronjob_update_endpoints')
+            except:
+                pass  # Handle any expected exceptions or log errors as needed
+
+    
