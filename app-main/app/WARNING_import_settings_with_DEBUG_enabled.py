0f7293c485b37beeb38e3acf7041c89d1def2ac9
Victor Reipur <vicre@dtu.dk>
2024-07-23 13:48:06 +0000

    added which user runs app

--- app-main/app/WARNING_import_settings_with_DEBUG_enabled.py
+++ app-main/app/WARNING_import_settings_with_DEBUG_enabled.py
@@ -1,7 +1,9 @@
 from .settings import *
 from termcolor import colored
+import getpass
 
 DEBUG = True
 
 if DEBUG:
-    print(colored('WARNING: DEBUG IS ENABLED, NEVER RUN IN PRODUCTION!', 'magenta'))
+    current_user = getpass.getuser()  # Get the current user's login name
+    print(colored(f'WARNING: DEBUG IS ENABLED, NEVER RUN IN PRODUCTION! Running as {current_user}', 'magenta'))
