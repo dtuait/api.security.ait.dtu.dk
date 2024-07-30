03a6ff12e760259b69eacb28c75a502d03ea5b1e
Victor Reipur <vicre@dtu.dk>
2024-07-30 12:00:26 +0000

    echo which user on startup

--- app-main/app/production_settings.py
+++ app-main/app/production_settings.py
@@ -1,19 +1,6 @@
 from .settings import *
+from termcolor import colored
+import getpass
 
-# DEBUG = True
-
-
-# CAS_SERVER_URL = 'https://auth.dtu.dk/dtu/' # no multifactor
-
-
-
-
-# CSRF_COOKIE_DOMAIN = 'localhost'
-
-# # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SESSION_COOKIE_SECURE = False
-# CSRF_COOKIE_SECURE = False
-# SECURE_HSTS_SECONDS = 0
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-# SECURE_HSTS_PRELOAD = False
-# SECURE_SSL_REDIRECT = False
+current_user = getpass.getuser()  # Get the current user's login name
+print(colored(f'Running as {current_user}', 'green'))
